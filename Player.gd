b4d9f225d77f8f79ba99136f2daa267aa354f909
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2023-12-20 11:11:21 -0800

    player scene complete

---
+++ Player.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var speed = 400.0
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
